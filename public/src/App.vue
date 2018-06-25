eaf48c64a50798afe46eb322ec86e7e8be34a668
Fabio Campos <fcampos@ncl.com>
2018-06-25 11:54:29 -0400

    + simpler side menu layout

--- public/src/App.vue
+++ public/src/App.vue
@@ -6,63 +6,28 @@
             fixed
             app
         >
-            <v-list dense>
-                <template v-for="item in items">
-                    <v-layout
-                        v-if="item.heading"
-                        :key="item.heading"
-                        row
-                        align-center
-                    >
-                        <v-flex xs6>
-                            <v-subheader v-if="item.heading">
-                                {{ item.heading }}
-                            </v-subheader>
-                        </v-flex>
-                        <v-flex xs6 class="text-xs-center">
-                            <a href="#!" class="body-2 black--text">EDIT</a>
-                        </v-flex>
-                    </v-layout>
-                    <v-list-group
-                        v-else-if="item.children"
-                        v-model="item.model"
-                        :key="item.text"
-                        :prepend-icon="item.model ? item.icon : item['icon-alt']"
-                        append-icon=""
-                    >
-                        <v-list-tile slot="activator">
-                            <v-list-tile-content>
-                                <v-list-tile-title>
-                                    {{ item.text }}
-                                </v-list-tile-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
-                        <v-list-tile
-                            v-for="(child, i) in item.children"
-                            :key="i"
-                            :to="{ path: item.path  }"
-                        >
-                            <v-list-tile-action v-if="child.icon">
-                                <v-icon>{{ child.icon }}</v-icon>
-                            </v-list-tile-action>
-                            <v-list-tile-content>
-                                <v-list-tile-title>
-                                    {{ child.text }}
-                                </v-list-tile-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
-                    </v-list-group>
-                    <v-list-tile v-else :key="item.text">
-                        <v-list-tile-action>
-                            <v-icon>{{ item.icon }}</v-icon>
-                        </v-list-tile-action>
+            <v-list>
+                <v-list-group
+                    v-for="item in items"
+                    v-model="item.active"
+                    :key="item.title"
+                    :prepend-icon="item.action"
+                    no-action
+                >
+                    <v-list-tile slot="activator">
                         <v-list-tile-content>
-                            <v-list-tile-title>
-                                {{ item.text }}
-                            </v-list-tile-title>
+                            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
                         </v-list-tile-content>
                     </v-list-tile>
-                </template>
+                    <v-list-tile v-for="subItem in item.items" :key="subItem.title" @click="">
+                        <v-list-tile-content>
+                            <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+                        </v-list-tile-content>
+                        <v-list-tile-action>
+                            <v-icon>{{ subItem.action }}</v-icon>
+                        </v-list-tile-action>
+                    </v-list-tile>
+                </v-list-group>
             </v-list>
         </v-navigation-drawer>
         <v-toolbar
@@ -195,38 +160,58 @@ export default {
             dialog: false,
             drawer: null,
             items: [
-                { icon: 'contacts', text: 'Contacts', path: '' },
-                { icon: 'history', text: 'Frequently contacted', path: '' },
-                { icon: 'content_copy', text: 'Duplicates', path: '' },
                 {
-                    icon: 'keyboard_arrow_up',
-                    'icon-alt': 'keyboard_arrow_down',
-                    text: 'Labels',
-                    model: true,
-                    children: [
-                        { icon: 'add', text: 'Create label', path: '' }
-                    ],
-                    path: ''
+                    action: 'local_activity',
+                    title: 'Attractions',
+                    items: [
+                        {title: 'List Item'}
+                    ]
                 },
                 {
-                    icon: 'keyboard_arrow_up',
-                    'icon-alt': 'keyboard_arrow_down',
-                    text: 'More',
-                    model: false,
-                    children: [
-                        { text: 'Import', path: '' },
-                        { text: 'Export', path: '' },
-                        { text: 'Print', path: '' },
-                        { text: 'Undo changes', path: '' },
-                        { text: 'Other contacts', path: '' }
-                    ],
-                    path: ''
+                    action: 'restaurant',
+                    title: 'Dining',
+                    active: true,
+                    items: [
+                        {title: 'Breakfast & brunch'},
+                        {title: 'New American'},
+                        {title: 'Sushi'}
+                    ]
                 },
-                { icon: 'settings', text: 'Settings', path: '' },
-                { icon: 'chat_bubble', text: 'Send feedback', path: '' },
-                { icon: 'help', text: 'Help', path: '' },
-                { icon: 'phonelink', text: 'App downloads', path: '' },
-                { icon: 'keyboard', text: 'Go to the old version', path: '' }
+                {
+                    action: 'school',
+                    title: 'Education',
+                    items: [
+                        {title: 'List Item'}
+                    ]
+                },
+                {
+                    action: 'directions_run',
+                    title: 'Family',
+                    items: [
+                        {title: 'List Item'}
+                    ]
+                },
+                {
+                    action: 'healing',
+                    title: 'Health',
+                    items: [
+                        {title: 'List Item'}
+                    ]
+                },
+                {
+                    action: 'content_cut',
+                    title: 'Office',
+                    items: [
+                        {title: 'List Item'}
+                    ]
+                },
+                {
+                    action: 'local_offer',
+                    title: 'Promotions',
+                    items: [
+                        {title: 'List Item'}
+                    ]
+                }
             ]
         }
     },
